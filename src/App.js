ad5e694cf4c286f8a44d91e1897e42429825562c
AnishaZ <anishagzacharia@gmail.com>
2018-04-02 14:45:42 -0700

    Body class: Added body class based on window.location.pathname

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 // local
@@ -12,23 +12,43 @@ import InsidePage from './pages/inside-page';
 import './App.css';
 
 
-const App = () => (
-  <div className="app body">
-    <div className="slide-bg">
-      <div className="wrapper">
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/about-us" component={InsidePage} />
-          <Route exact path="/about-us" component={InsidePage} />
-          <Route exact path="/classes" component={InsidePage} />
-          <Route exact path="/media" component={InsidePage} />
-          <Route exact path="/contact" component={InsidePage} />
-        </Switch>
-        <Footer />
+class App extends Component {
+  constructor() {
+    super();
+    this.location = this.location.bind(this);
+  }
+
+  location() {
+    const url = window.location.pathname;
+    if (url !== '/') {
+      this.locationClass = url.substring(1);
+    } else {
+      this.locationClass = 'home';
+    }
+    return this.locationClass;
+  }
+
+  render() {
+    const locationClass = this.location();
+    return (
+      <div className={`app body ${locationClass}`}>
+        <div className="slide-bg">
+          <div className="wrapper">
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/about-us" component={InsidePage} />
+              <Route exact path="/about-us" component={InsidePage} />
+              <Route exact path="/classes" component={InsidePage} />
+              <Route exact path="/media" component={InsidePage} />
+              <Route exact path="/contact" component={InsidePage} />
+            </Switch>
+            <Footer />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default App;
