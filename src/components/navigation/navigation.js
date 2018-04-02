ca71b1cc8d02ce684cee0f03c0b8cde5b993a306
AnishaZ <anishagzacharia@gmail.com>
2018-04-02 11:35:29 -0700

    Header nav: Selected class fix for React-Router-4

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './nav.css';
+
 const Nav = props => (
   <ul className={props.cssClass}>
     {props.children}
--- src/components/navigation/navigationLink.js
+++ src/components/navigation/navigationLink.js
@@ -3,11 +3,16 @@ import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const NavigationLink = props => (
-  <NavLink to={props.link} activeClassName="current">{props.children}</NavLink> // eslint-disable-line
+  <NavLink to={props.link} activeClassName="current" exact={props.exact}>{props.children}</NavLink>
 );
+
+NavigationLink.defaultProps = {
+  exact: '',
+};
 
 NavigationLink.propTypes = {
   link: PropTypes.string.isRequired,
+  exact: PropTypes.string,
   children: PropTypes.string.isRequired,
 };
 export default NavigationLink;
