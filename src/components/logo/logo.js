d1dd38a2c411182d1eb4257024ea7d69a893482a
AnishaZ <anishagzacharia@gmail.com>
2018-04-02 11:46:56 -0700

    Logo: Switching to <Link> from React-Router-4

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../logo/logo.css';
+
 
 const Logo = () => (
   <div className="logo">
-    <h1>Mayura Dance | Indian, Bollywood Dance Classes Los Angeles</h1>
+    <Link to="/" title="Mayura Dance">
+      <h1>Mayura Dance | Indian, Bollywood Dance Classes Los Angeles</h1>
+    </Link>
   </div>
 );
 
