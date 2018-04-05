172e1e92fece8be54215b41bd363f7f206e9663f
AnishaZ <anishagzacharia@gmail.com>
2018-04-04 21:53:28 -0700

    Iterating over child nodes for footer menus

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,9 +5,36 @@ import NavigationLink from '../navigation/navigationLink';
 
 
 class Footer extends Component {
+  constructor() {
+    super();
+    this.updateKeys = this.updateKeys.bind(this);
+    this.traverse = this.traverse.bind(this);
+  }
+
+  traverse(obj) {
+    const objString = Object.prototype.toString;
+    if (objString.call(obj) === '[object Array]') {
+      for (let i = 0; i < obj.length; i++) {
+        console.log(obj[i].title);
+        this.traverse(obj[i]);
+      }
+    } else {
+      for (const key in obj) {
+        if (objString.call(obj[key]) === '[object Array]') {
+          this.traverse(obj[key]);
+        }
+      }
+    }
+  }
+
+  updateKeys(api) {
+    this.traverse(api);
+  }
+
   render() {
     const footerClass = 'nav nav__footer';
     const { items } = this.props;
+    this.updateKeys(items);
     return (
       <Nav cssClass={footerClass}>
         {Object
